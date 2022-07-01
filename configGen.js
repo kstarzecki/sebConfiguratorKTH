fee78e85912c9a74f1831ef2da06933d6c988596
kstarzecki <k.starzecki@gmail.com>
2022-07-01 17:37:51 +0200

    rules update

--- configGen.js
+++ configGen.js
@@ -8,7 +8,27 @@ function generateSEBConfig (courseID) {
     let ruleString = ''
     const ruleArr = []
   
-    const regexArr = ['([\\w\\d]+\\.)?canvas\\.kth\\.se(\\/)?$', `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses\\/${courseID}$`, `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses\\/${courseID}\\/assignments(\\/.*)?$`, `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses\\/${courseID}\\/external_tools\\/retrieve\\?display=full_width&url=https%3A%2F%2Fkth.quiz-lti-dub-prod.instructure.com(.*)?$`, `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses\\/${courseID}\\/modules(\\/.*)?$`, `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses\\/${courseID}\\/modules/items/([0-9]+)?$`, `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses\\/${courseID}\\/modules\\#module_([0-9]+)$`, `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses\\/${courseID}\\/pages\\/([a-zA-Z0-9_.-]+)\\?module_item_id=([0-9]+)$`, `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses\\/${courseID}\\/quizzes(\\/.*)?$`, `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses\\/${courseID}\\/student_view(\\/.*)?$`, `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses\\/${courseID}\\/test_student(\\/.*)?$`, '([\\w\\d]+\\.)?canvas\\.kth\\.se\\/login(\\/.*)?$', '([\\w\\d]+\\.)?kth\\.mobius\\.cloud(\\/.*)?$', '([\\w\\d]+\\.)?login\\.sys\\.kth\\.se(\\/.*)?$', '([\\w\\d]+\\.)?login\\.ug\\.kth\\.se(\\/.*)?$', '([\\w\\d]+\\.)?saml-5\\.sys\\.kth\\.se(\\/.*)?$', '([\\w\\d]+\\.)?saml-5\\.ug\\.kth\\.se(\\/.*)?$', '([\\w\\d]+\\.)?sso\\.canvaslms\\.com\\/delegated_auth_pass_through\\?target=(.*)$', `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses\\/${courseID}\\/files\\/([a-zA-Z0-9_.-]+)\\?module_item_id=([0-9]+)$`]
+    const regexArr = [
+      '([\\w\\d]+\\.)?canvas\\.kth\\.se(\\/)?$',
+      `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses/${courseID}?$`,
+      `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses/${courseID}\\/assignments(\\/.*)?$`,
+      `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses/${courseID}\\/external_tools\\/retrieve\\?display=full_width&amp,url=https%3A%2F%2Fkth.quiz-lti-dub-prod.instructure.com(.*)?$`,
+      `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses/${courseID}\\/modules(\\/.*)?$`,
+      `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses/${courseID}\\/modules/items/([0-9]+)?$`,
+      `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses/${courseID}\\/modules\\#module_([0-9]+)$`,
+      `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses/${courseID}\\/pages\\/([a-zA-Z0-9_.-]+)\\?module_item_id=([0-9]+)$`,
+      `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses/${courseID}\\/quizzes(\\/.*)?$`,
+      `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses/${courseID}\\/student_view(\\/.*)?$`,
+      `([\\w\\d]+\\.)?canvas\\.kth\\.se\\/courses/${courseID}\\/test_student(\\/.*)?$`,
+      '([\\w\\d]+\\.)?canvas\\.kth\\.se\\/login(.*?)$',
+      '([\\w\\d]+\\.)?kth\\.mobius\\.cloud(\\/.*)?$',
+      '([\\w\\d]+\\.)?login\\.sys\\.kth\\.se(.*?)$',
+      '([\\w\\d]+\\.)?login\\.ug\\.kth\\.se(.*?)$',
+      '([\\w\\d]+\\.)?saml-5\\.sys\\.kth\\.se(.*?)$',
+      '([\\w\\d]+\\.)?saml-5\\.ug\\.kth\\.se(.*?)$',
+      '([\\w\\d]+\\.)?sso\\.canvaslms\\.com\\/delegated_auth_pass_through\\?target=(.*)$',
+      '([\\w\\d]+\\.)?canvas\\.kth\\.se\\/logout(.*?)$'
+    ]
   
     const stencilRuleObj = {
       type: 'element',
